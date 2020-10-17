56760e9ec9863eb99f84dd9e8907a149bd63d12e
EveDavidLouis <eve.david.louis@gmail.com>
2020-10-17 08:07:03 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+import logging
+import json
+
+logger = logging.getLogger('app')
+logging.basicConfig(level=logging.WARNING)
+
+if __name__ == "__main__":
+
+  for k, v in sorted(os.environ.items()):
+    logger.warning(k+':', v)
+  
+  print('Hello world')
