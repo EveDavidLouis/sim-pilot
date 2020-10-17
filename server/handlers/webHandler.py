010bbe008096dcf5e9248ce353827d12ed47c6e7
EveDavidLouis <eve.david.louis@gmail.com>
2020-10-17 09:48:10 +0200

    Update webHandler.py

--- server/handlers/webHandler.py
+++ server/handlers/webHandler.py
@@ -1,8 +1,14 @@
 import json
 from tornado import web
 
+class DefaultHTMLHandler(web.RequestHandler):
+	async def get(self, *args, **kwargs):
+		data = "DefaultHTMLHandler"
+		self.write(str(data))
+		self.finish()
+
 class DefaultJSONHandler(web.RequestHandler):
 	async def get(self,args):
-		data = {'data':args}
+		data = {'DefaultJSONHandler':args}
 		self.write(json.dumps(data))
 		self.finish()
