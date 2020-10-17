2182847fc1af01a2b7828f4ce22be92fb372b80e
EveDavidLouis <eve.david.louis@gmail.com>
2020-10-17 08:09:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ logging.basicConfig(level=logging.WARNING)
 if __name__ == "__main__":
 
   for k, v in sorted(os.environ.items()):
-    logger.warning(k+':', v)
+    logger.warning(k+':', str(v))
   
   print('Hello world')
