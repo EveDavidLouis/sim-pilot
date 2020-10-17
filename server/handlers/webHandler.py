dee3606b96479105810255662b17ede7e3f4316b
EveDavidLouis <eve.david.louis@gmail.com>
2020-10-17 08:40:21 +0200

    Update webHandler.py

--- server/handlers/webHandler.py
+++ server/handlers/webHandler.py
@@ -1,12 +1,12 @@
+from tornado import web
+
 class DefaultHTMLHandler(web.RequestHandler):
-	@gen.coroutine
-	def get(self,args):
+	async def get(self,args):
 		self.write(self.render('../../docs/index.html'))
 		self.finish()
 
 class DefaultJSONHandler(web.RequestHandler):
-	@gen.coroutine
-	def get(self,args):
+	async def get(self,args):
 		data = {}
 		self.write(json.dumps(data))
 		self.finish()
