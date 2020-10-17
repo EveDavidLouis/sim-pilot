bd8ee5397a5efe65523efd47f5c4ab21ed9fec67
EveDavidLouis <eve.david.louis@gmail.com>
2020-10-17 08:21:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import os
 import logging
 import json
 
+from tornado import ioloop , gen , web
+from motor.motor_tornado import MotorClient
 from server import config
+from server.handlers import webHandler
 
 logger = logging.getLogger('app')
 logging.basicConfig(level=logging.WARNING)
